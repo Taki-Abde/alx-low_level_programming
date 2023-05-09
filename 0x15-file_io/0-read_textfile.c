3912ff4eee3fcfc7e58024e3c0ec943e151c73bf
Taki-Abde <takiabderrahim005@gmail.com>
2023-05-09 05:14:42 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,30 +1,33 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to standard output
- * @filename: file descripter
- * @letters: count
- * Return: any time size_t
+ * read_textfile - reads text file and prints it to POSIX stdout.
+ * @filename: pointer to name of file.
+ * @letters: number of letters func and print.
+ * Return: If the function fails or filename return 0.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, sz, oz;
-	char *buf = malloc(sizeof(char) * letters);
+	ssize_t opened, wrote, reading;
+	char *buffer;
 
-	fd = open(filename, O_RDONLY);
-	if ((filename == NULL) || (fd == -1) || (buf == NULL))
-	{
-		free(buf);
+	if (filename == NULL)
 		return (0);
-	}
-	sz = read(fd, buf, letters);
-	if (sz == -1)
+	opened = open(filename, O_RDONLY);
+	if (opened == -1)
 		return (0);
-	buf[sz] = '\0';
-	oz = write(STDOUT_FILENO, buf, sz);
-	if (oz == -1)
+	buffer = malloc(letters);
+	if (buffer == NULL)
 		return (0);
-	close(fd);
-	return (oz);
+	reading = read(opened, buffer, letters);
+	if (reading == -1)
+		return (0);
+	wrote = write(STDOUT_FILENO, buffer, reading);
+	if (wrote == -1 || wrote != reading)
+		return (0);
+	close(opened);
+	free(buffer);
+	return (wrote);
 }
